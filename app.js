d2008f74f56b16cb009f440a03bee7b0c29fe85e
a-steel <alpkaanc13@gmail.com>
2016-07-25 22:37:13 -0700

    Hello Azure App Service

---
+++ app.js
@@ -0,0 +1,53 @@
+var builder = require('botbuilder');
+var restify = require('restify'):
+
+//Server Setup
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+//Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID, //feaa42f3-e01e-4149-b90b-56bcaec38070
+    appPassword: process.env.MICROSOFT_APP_PASSWORD //NegWuJZmtpMvX4XKftem7oc
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//Dialog
+var intents = new builder.IntentDialog();
+
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
